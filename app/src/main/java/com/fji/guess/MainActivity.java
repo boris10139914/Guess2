2c6da313159b55c0144f0b1f48219127f6adab15
harris <harris2030057>
2017-11-10 14:54:10 +0800

    guassV2

--- app/src/main/java/com/fji/guess/MainActivity.java
+++ app/src/main/java/com/fji/guess/MainActivity.java
@@ -5,38 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Random ran = new Random();
-    int i1 = ran.nextInt(9)+1;
-    @Override
+    int i1 = ran.nextInt(10)+1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      Button bSend = (Button)findViewById(R.id.b_send);
     }
 
-public void guess(View view){
 
-    EditText ednumber = (EditText) findViewById(R.id.ed_number);
-    int i2 = Integer.parseInt(ednumber.getText().toString());
+    public void guess(View view) {
+
+        EditText ednumber = (EditText) findViewById(R.id.ed_number);
+        int i2 = Integer.parseInt(ednumber.getText().toString());
+        if(i1<i2){
+            Toast.makeText(MainActivity.this, "小一點", Toast.LENGTH_SHORT).show();
+        }
+        if(i1>i2){
+            Toast.makeText(MainActivity.this, "大一點", Toast.LENGTH_SHORT).show();
+        }
+        if(i1==i2){
+            Toast.makeText(MainActivity.this, "答對了", Toast.LENGTH_SHORT).show();
+        }
 
 
-    if(i2<i1){
-        new AlertDialog.Builder(this)
-                .setMessage("小一點")
-                .show();
-    }else if (i2>i1){
-        new AlertDialog.Builder(this)
-                .setMessage("大一點")
-                .show();
-    } else {
-        new AlertDialog.Builder(this)
-                .setMessage("答對了")
-                .show();
     }
 
-}}
+}
 
 
